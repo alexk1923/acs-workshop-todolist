2cb795e0717ca050501a7280c3ec4b51ae8ff458
Alex Kullman <alexkullman@MacBook-Pro-Alex-2.local>
2024-12-09 21:25:00 +0200

    Initial changes from prev project

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,350 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import trashIcon from "./assets/trash.png";
+import giftIcon from "./assets/giftbox.png";
+import {
+	MdOutlineCheckBox,
+	MdOutlineCheckBoxOutlineBlank,
+} from "react-icons/md";
+import "./App.css";
+import api from "./api/api";
+
+const STATUSES = {
+	ALL: "ALL",
+	PENDING: "Pending",
+	SEARCHING: "In progress",
+	BOUGHT: "Completed",
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [wishes, setWishes] = useState([]);
+	const [newWish, setNewWish] = useState("");
+	const [newDescription, setNewDescription] = useState("");
+	const [newAssignee, setNewAssignee] = useState("All");
+	const [newDueDate, setNewDueDate] = useState(null);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const [initialTasks, setInitialTasks] = useState([]);
+	const [currentStatus, setCurrentStatus] = useState(STATUSES.ALL);
+	const [filteredWishes, setFilteredWishes] = useState([]);
+
+	const [guests, setGuests] = useState([]);
+	const [currentAssignee, setCurrentAssignee] = useState(-1);
+
+	const getAllGuests = async () => {
+		const fetchedGuests = await api.getGuests();
+		setGuests(fetchedGuests);
+	};
+
+	const getAllWishes = async () => {
+		const fetchedWishes = await api.getTasks();
+		setWishes(fetchedWishes);
+		setFilteredWishes(fetchedWishes);
+		setInitialTasks(fetchedWishes);
+	};
+
+	useEffect(() => {
+		getAllWishes();
+		getAllGuests();
+	}, []);
+
+	const filterWishes = (status) => {
+		setCurrentStatus(status);
+		if (status === STATUSES.ALL) {
+			setFilteredWishes(wishes);
+			return;
+		}
+		setFilteredWishes(wishes.filter((w) => w.status === status));
+	};
+
+	useEffect(() => {
+		console.log("");
+		filterWishes(currentStatus);
+	}, [wishes]);
+
+	const handleRemoveBoughtWishes = () => {
+		setWishes(wishes.filter((w) => w.status !== STATUSES.BOUGHT));
+	};
+
+	const handleRemoveWish = async (id) => {
+		// setWishes(wishes.filter((w) => w.id !== id));
+		await api.deleteTask(id);
+		getAllWishes();
+	};
+
+	const addNewWish = async () => {
+		if (newWish.length < 1 || newAssignee.length < 1) {
+			return;
+		}
+
+		console.log(newWish);
+		console.log(newDescription);
+		console.log(newAssignee);
+		console.log(newDueDate);
+
+		const newTask = {
+			title: newWish,
+			description: newDescription,
+			guest: { id: newAssignee },
+			dueDate: newDueDate,
+			status: STATUSES.PENDING,
+		};
+
+		await api.createTask(newTask);
+		setNewWish("");
+		getAllWishes();
+	};
+
+	const handleMarkWish = async (id) => {
+		// const newWishes = wishes.map((w) => {
+		// 	if (w.id !== id) {
+		// 		return w;
+		// 	} else {
+		// 		return {
+		// 			...w,
+		// 			status:
+		// 				w.status === STATUSES.BOUGHT ? STATUSES.PENDING : STATUSES.BOUGHT,
+		// 		};
+		// 	}
+		// });
+		// console.log("new wishes");
+		// console.log(newWishes);
+
+		const newWish = wishes.find((w) => w.id == id);
+		console.log("The new wish is:");
+		console.log(newWish);
+
+		if (newWish) {
+			await api.updateTask(id, {
+				...newWish,
+				status:
+					newWish.status === STATUSES.BOUGHT
+						? STATUSES.PENDING
+						: STATUSES.BOUGHT,
+			});
+		}
+
+		// setWishes(newWishes);
+		getAllWishes();
+	};
+
+	const handleSelectListByGuest = (guest) => {
+		const newVal = guest;
+
+		if (currentAssignee === newVal.id) {
+			setCurrentAssignee(-1);
+			return;
+		}
+
+		setCurrentAssignee(guest.id);
+	};
+
+	useEffect(() => {
+		if (currentAssignee === -1) {
+			getAllWishes();
+			return;
+		}
+
+		setWishes(initialTasks.filter((w) => w.guest.id === currentAssignee));
+	}, [currentAssignee]);
+
+	return (
+		<>
+			<div className='hero-section'>
+				<h1>New Year List</h1>
+			</div>
+			<div className='page-container'>
+				<div>
+					<div className='new-task-container'>
+						<input
+							placeholder='Create new task...'
+							type='text'
+							onChange={(e) => {
+								setNewWish(e.target.value);
+								console.log(e.target.value);
+							}}
+							className='wish-input'
+							value={newWish}
+						/>
+
+						<textarea
+							placeholder='Description...'
+							onChange={(e) => {
+								setNewDescription(e.target.value);
+								console.log(e.target.value);
+							}}
+							className='task-description-input'
+							value={newDescription}
+						/>
+
+						<div>
+							<span style={{ color: "black" }}>Assignee:</span>
+							<select
+								value={newAssignee}
+								onChange={(e) => setNewAssignee(e.target.value)}
+							>
+								{guests.map((guest) => (
+									<option value={guest.id}>
+										{guest.firstName} {guest.lastName}
+									</option>
+								))}
+							</select>
+						</div>
+
+						<div>
+							<label htmlFor='dueDate' style={{ color: "black" }}>
+								Due date
+							</label>
+							<input
+								type='date'
+								name='dueDate'
+								id='dueDate'
+								value={newDueDate}
+								onChange={(e) => setNewDueDate(e.target.value)}
+							/>
+						</div>
+
+						<button onClick={addNewWish}>Add</button>
+					</div>
+					<div className='wishes-card'>
+						<div className='wish-list'>
+							{filteredWishes.map(
+								({ id, title, status, description, guest, dueDate }) => (
+									<div className='wish-row'>
+										<div className='wish-entry'>
+											<div className='wish-container' key={id}>
+												<div className='task-title-container'>
+													<div
+														onClick={() => handleMarkWish(id)}
+														style={{
+															cursor: "pointer",
+															display: "flex",
+															aspectRatio: 1 / 1,
+														}}
+													>
+														{status === STATUSES.BOUGHT ? (
+															<MdOutlineCheckBox />
+														) : (
+															<MdOutlineCheckBoxOutlineBlank />
+														)}
+													</div>
+													<span
+														className={
+															status === STATUSES.BOUGHT ? "bought-wish" : ""
+														}
+													>
+														{title}
+													</span>
+												</div>
+
+												<span className='task-description'>
+													Assigned to {guest.firstName} {guest.lastName}
+												</span>
+
+												<span className='task-description'>
+													Due to {new Date(dueDate).toLocaleDateString()}
+												</span>
+												<span className='task-description'>{description}</span>
+											</div>
+										</div>
+										<img
+											src={trashIcon}
+											onClick={() => handleRemoveWish(id)}
+											width={32}
+											height={32}
+										/>
+
+										{/* {optionsVisible && (
+											<div className='options-menu'>
+												<span style={{ color: "black" }}>Edit</span>
+												<span
+													style={{ backgroundColor: "red", color: "white" }}
+												>
+													Delete
+												</span>
+											</div>
+										)} */}
+
+										{/* <img
+									src={trashIcon}
+									style={{
+										width: 32,
+										height: 32,
+										marginRight: "1rem",
+									}}
+									onClick={() => {
+										handleRemoveWish(id);
+									}}
+								/> */}
+									</div>
+								)
+							)}
+						</div>
+
+						<div className='wish-list-footer'>
+							<span>{filteredWishes.length} items left</span>
+							<div className='statuses'>
+								<span
+									onClick={() => filterWishes(STATUSES.ALL)}
+									className={
+										currentStatus === STATUSES.ALL
+											? "status selected-status"
+											: "status"
+									}
+								>
+									All
+								</span>
+								<span
+									onClick={() => filterWishes(STATUSES.PENDING)}
+									className={
+										currentStatus === STATUSES.PENDING
+											? "status selected-status"
+											: "status"
+									}
+								>
+									Pending
+								</span>
+								<span
+									onClick={() => filterWishes(STATUSES.SEARCHING)}
+									className={
+										currentStatus === STATUSES.SEARCHING
+											? "status selected-status"
+											: "status"
+									}
+								>
+									Searching
+								</span>
+								<span
+									onClick={() => filterWishes(STATUSES.BOUGHT)}
+									className={
+										currentStatus === STATUSES.BOUGHT
+											? " status selected-status"
+											: "status"
+									}
+								>
+									Bought
+								</span>
+							</div>
+
+							<span onClick={handleRemoveBoughtWishes}>Clear bought ones</span>
+						</div>
+					</div>
+				</div>
+
+				<div className='list-by-guest'>
+					<h2>View specific list</h2>
+					{guests.map((guest) => (
+						<div
+							className={
+								currentAssignee === guest.id ? "item selected-item" : "item"
+							}
+							onClick={() => handleSelectListByGuest(guest)}
+						>
+							{guest.firstName} {guest.lastName}
+						</div>
+					))}
+				</div>
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
---
+++ src/api/api.js
@@ -0,0 +1,100 @@
+import { API_URL } from "./config";
+
+async function getGuests() {
+	try {
+		const response = await fetch(`${API_URL}/guests`);
+		if (!response.ok) throw new Error("Failed to fetch GUESTS");
+
+		const data = await response.json();
+		console.log("Guests:", data);
+		return data;
+	} catch (error) {
+		console.error("Error fetching guests:", error);
+	}
+}
+
+async function updateTask(taskId, updatedTask) {
+	try {
+		console.log("Updating task...");
+
+		const response = await fetch(`${API_URL}/tasks/${taskId}`, {
+			method: "PUT",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(updatedTask),
+		});
+
+		if (!response.ok) throw new Error("Failed to update task");
+
+		const data = await response.json();
+		console.log("Updated task:", data);
+		return data;
+	} catch (error) {
+		console.error("Error adding task:", error);
+	}
+}
+
+async function getTasks() {
+	try {
+		const response = await fetch(`${API_URL}/tasks`, {
+			method: "GET",
+		});
+		if (!response.ok) {
+			console.log(response);
+
+			throw new Error("Failed to fetch TASKS");
+		}
+
+		const data = await response.json();
+		console.log("Tasks:", data);
+		return data;
+	} catch (error) {
+		console.error("Error fetching tasks:", error);
+	}
+}
+
+async function createTask(task) {
+	try {
+		const response = await fetch(`${API_URL}/tasks`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(task),
+		});
+
+		if (!response.ok) {
+			throw new Error("Failed to add task");
+		}
+
+		const data = await response.json();
+		console.log("New Task:", data);
+		return data;
+	} catch (error) {
+		console.error("Error adding task:", error);
+	}
+}
+
+async function deleteTask(taskId) {
+	try {
+		const response = await fetch(`${API_URL}/tasks/${taskId}`, {
+			method: "DELETE",
+		});
+		if (!response.ok) throw new Error("Failed to delete TASK");
+
+		return;
+	} catch (error) {
+		console.error("Error deleting Task", error);
+	}
+}
+
+const api = {
+	getGuests,
+	getTasks,
+	createTask,
+	deleteTask,
+	updateTask,
+};
+
+export default api;
